4139223671b69204cbb7e063e74eb43a405b61ed
SciutoAlex <SciutoAlex@gmail.com>
2014-05-10 20:48:18 -0500

    updated page to allow for custom js

--- page.php
+++ page.php
@@ -1,37 +1,50 @@
-<?php 
-  
+<?php
+
   if ($_SERVER['SERVER_NAME'] === "localhost") {
     define("ROOT_URL", "/github-alex");
   } else {
-    define("ROOT_URL", ""); 
+    define("ROOT_URL", "");
   }
-  
-  
-  
+
+
+
   function page_header($opts) { ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <title>AlexSciuto.com</title>
+    <meta charset="utf-16"/>
+    <?
+      if(!$opts['title']) {
+        $opts['title'] = "AlexSciuto.com";
+      }
+    ?>
+    <title><?= $opts['title'] ?></title>
     <meta name="viewport" content="width=device-width, user-scalable=false;">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="<?=ROOT_URL;?>/styles/main.css" />
     <script type="text/javascript" src="//use.typekit.net/vwe0ulk.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     <script src="<?=ROOT_URL;?>/js/mustache.js"></script>
+
+    <? foreach($opts["css"] as $path) { ?>
+        <link rel="stylesheet" type="text/css" href="<?=$path;?>" />
+    <? } ?>
+
+    <? foreach($opts["js"] as $path) { ?>
+        <script type"text/javascript" src="<?=$path;?>" /></script>
+    <? } ?>
     <script type="text/javascript">
-      
+
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-10951674-4']);
       _gaq.push(['_trackPageview']);
-      
+
       (function() {
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
       })();
-      
+
     </script>
     <script src="<?=ROOT_URL;?>/js/image-lens.js"></script>
   <script>
@@ -43,15 +56,15 @@
   </head>
   <body>
     <div id="container">
-      
+
       <?php nav($opts); ?>
-      
-      
-      
+
+
+
       <?php
 }
-  
-  
+
+
   function nav($opts) {
    ?>
       <div id="top-info-mobile">
@@ -65,21 +78,21 @@
         <div id="top-info">
           <div style="background-color:#fff; width:1030px;">
             <div class="fleft">
-              
+
               <h1 class="logo">
                 <a href="/">
                   <span>&nbsp; Interaction Design  and Data Visualization</span><img src="<?=ROOT_URL;?>/img/alex-logo.jpg" width="90"></a></h1>
             </div>
-            
+
             <div class="fright">
               <ul class="nav">
-               
+
                 <li>
                   <a href="http://www.dribbble.com/sciutoalex/"><img src="<?=ROOT_URL;?>/img/dribbble.jpg" alt="" width="23"/></a>
-                </li> 
+                </li>
                  <li>
                    <a href="http://www.twitter.com/sciutoalex/"><img src="<?=ROOT_URL;?>/img/twitter.jpg" alt="" width="29" /></a>
-                </li> 
+                </li>
                 <li>
                   <p class="small"><a href="<?=ROOT_URL;?>/blog">Blog</a></p>
                 </li>
@@ -93,33 +106,33 @@
                 </li>
               </ul>
             </div>
-           <div class="clear"></div>  
-            
+           <div class="clear"></div>
+
           </div>
-          <div class="gradient" style="display:none; height:4px; width:1030px;"></div>   
-         <div class="clear"></div>  
+          <div class="gradient" style="display:none; height:4px; width:1030px;"></div>
+         <div class="clear"></div>
         </div>
-         
+
       </div>
-      
-      
-     
+
+
+
      <?php
-    
+
   }
-      
-  function page_bottom() { 
-    
-    
+
+  function page_bottom() {
+
+
     ?>
-  
-      
-      
+
+
+
     </div>
-    
-    
+
+
     <script type="text/javascript">
-       
+
         $(document).ready(function() {
           var logoTransition = false;
           var clone;
@@ -129,13 +142,13 @@
           var $logoimg = $logo.find('img');
           var $logospan = $logo.find('span');
           var speed = 200;
-          
+
           $(window).scroll(function() {
             if(($(window).scrollTop() > 110) && logoTransition == false) {
               $('.gradient').fadeIn();
               logoTransition = true;
-              
-              
+
+
               $topInfo.css('margin', 0);
               $logo.css('margin-top', 5);
               $topInfo.css('position', 'fixed');
@@ -144,7 +157,7 @@
               $topInfo.css('margin', 0);
               $topInfo.css('width', 1030);
               $logospan.fadeOut(speed, function() {
-                
+
                  $nav.animate({
                 'padding': '20px 00px 40px 00px'
               }, speed);
@@ -154,17 +167,17 @@
                   height : 30
                 }, speed, function() {
                   $logoimg.attr('src', '<?=ROOT_URL;?>/img/alex-logo-off.jpg');
-                  
+
                 });
               });
 
               $logospan.animate({
                 'padding': '4px'
               }, speed);
-              
-              
+
+
             } else if(($(window).scrollTop() < 110) && logoTransition == true) {
-              
+
               logoTransition = false;
               $('.gradient').fadeOut();
               $topInfo.css('position', 'static');
@@ -172,46 +185,46 @@
               $topInfo.css('top', 0);
               $topInfo.css('margin', 0);
               $topInfo.css('width', 1030);
-              
+
               $logospan.fadeOut(speed, function() {
-                
+
                  $nav.animate({
                 'padding': '45px 00px 40px 00px'
               }, speed);
-                
+
                 $logospan.html('&nbsp;&nbsp;Interaction Design and Prototyping').fadeIn();
                 $logoimg.animate({
                   width : 93,
                   height : 93
                 }, speed, function() {
                   $logoimg.attr('src', '<?=ROOT_URL;?>/img/alex-logo.jpg');
-                  
+
                 });
               });
               $logospan.animate({
                 'padding': '37px 0px'
               }, speed);
-            } 
+            }
           });
            });
-        
-        
-        
+
+
+
     </script>
-      
+
     </body>
     </html>
-      <?php 
+      <?php
   }
-  
+
   function page_about() { ?>
       <div id="shadow">
         <div id="about">
           <button class="closewindow"></button>
           <h1>Hi, I'm Alex Sciuto.</h1>
-          
+
           <div class="col-l">
-            
+
             <p> I design and code websites. I love thinking about user-centered design, and I really like playing with sets of data.</p>
             <p>A few facts about me:
               <ul>
@@ -221,7 +234,7 @@
                 <li>I have a younger sister. Her name is Ellen, and she's pretty great.</li>
               </ul>
             </p>
-            <p>Some people have written nice things about my work: <a href="http://ftalphaville.ft.com/blog/2012/07/18/1088201/happy-2nd-birthday-dodd-frank/">Financial Times</a> and <a href="http://www.forbes.com/sites/prishe/2012/05/09/the-correlation-between-impulse-nfl-ticket-purchases-and-2012-nfl-draft-selections/">Forbes</a>. 
+            <p>Some people have written nice things about my work: <a href="http://ftalphaville.ft.com/blog/2012/07/18/1088201/happy-2nd-birthday-dodd-frank/">Financial Times</a> and <a href="http://www.forbes.com/sites/prishe/2012/05/09/the-correlation-between-impulse-nfl-ticket-purchases-and-2012-nfl-draft-selections/">Forbes</a>.
             </p>
           </div>
           <div class="col-r">
@@ -237,16 +250,16 @@
           <div class="clear"></div>
         </div>
       </div>
-      <?php 
+      <?php
 }
-  
-  function page_menu() { 
-    
+
+  function page_menu() {
+
     $files = glob("*.php");
-    
+
     echo "<pre>";
-    
-    
+
+
     echo "<ul id='menu'>";
     foreach($files as $file) {
       $display_name = str_replace("-"," ",$file);
@@ -254,18 +267,18 @@
       echo "<li><a href='".$file."'>".$display_name."</a></li>";
     }
     echo "</ul></pre>";
-    
+
   }
-  
+
   function instagram_stream() {
     $id = "fd130264fc134ca8985338045d59f845";
-    $CLIENT_SECRET = "18d5378bad4b4ff8a66cfbd6c33c0a44"; 
-    $auth_token = "243814811.ab103e5.4042aa59a025416eaefb165561e44471";  
+    $CLIENT_SECRET = "18d5378bad4b4ff8a66cfbd6c33c0a44";
+    $auth_token = "243814811.ab103e5.4042aa59a025416eaefb165561e44471";
     $user = "243814811";
     $count = 7;
-    
+
     $url = "https://api.instagram.com/v1/users/$user/media/recent/?access_token=$auth_token&count=$count";
-    
+
     $ch = curl_init();
     $timeout = 5;
     curl_setopt($ch,CURLOPT_URL, $url);
@@ -275,13 +288,13 @@
     curl_close($ch);
     //echo $data;
     $data = json_decode($data);
-    
-    
-    
+
+
+
     foreach ($data->data as $post) {
       echo "<a target=_blank class='instagramphoto' href=".$post->link."><img width='144' heigh='144' src=".$post->images->thumbnail->url."></a>";
     }
-    
+
   }
-  
+
   ?>
